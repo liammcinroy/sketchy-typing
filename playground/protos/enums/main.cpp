64c8be47b065181a35e7ad90191ec15379707523
William McInroy <mcinroy@college.harvard.edu>
2022-12-03 11:06:21 -0500

    starting to add in simplicial identity conditions (errors)

--- playground/protos/enums/main.cpp
+++ playground/protos/enums/main.cpp
@@ -27,8 +27,194 @@ using test1 = IsEnum<Vertices>;
 namespace simpsets {
 
 namespace details {
+
+//// Basic usual index variadic
+namespace get {
+template <size_t i, typename T0, typename... Ts>
+struct f {
+  using type = f<i - 1, Ts...>;
+};
+
+template <typename T0, typename... Ts>
+struct f<0, T0, Ts...> {
+  using type = T0;
+};
+
+template <size_t i, typename... Ts>
+using at = typename f<i, Ts...>::type;
+}  // namespace get
+
+// Implements `_Pack`
+template <typename... _Ts>
+struct Pack {
+  static constexpr size_t n = sizeof...(_Ts);
+
+  template <size_t i>
+  using at = typename std::
+      conditional_t<0 <= i && i <= n - 1, get::at<i, _Ts...>, void>;
+
+  template <template <typename...> class T, typename... _Ts2>
+  struct postapply {
+    using type = T<_Ts2..., _Ts...>;
+  };
+
+  template <template <typename...> class T, typename... _Ts2>
+  struct preapply {
+    using type = T<_Ts..., _Ts2...>;
+  };
+
+  // TODO: maybe make `apply` enhance to `T` with `unapply`?
+  // make this whole idea a lil more rigorous.
+};
+
+template <typename T>
+concept _Pack = requires {
+  T::template at<size_t>;
+
+  { T::n } -> std::same_as<const size_t>;  // does work!
+};
+
+//// some sort of meaningful `for`
+namespace forloop {
+template <size_t start,
+          size_t end,
+          _Pack pack,
+          template <typename...>
+          class constr,
+          bool cont>
+struct impl {};
+
+template <size_t start,
+          size_t end,
+          _Pack pack,
+          template <typename...>
+          class constr>
+struct impl<start, end, pack, constr, false> {
+  using type = constr<>;
+};
+
+template <size_t start,
+          size_t end,
+          _Pack pack,
+          template <typename...>
+          class constr>
+struct impl<start, end, pack, constr, true> {
+  using type =
+      typename impl<start + 1, end, pack, constr, start + 1 <= end - 1>::type::
+          template postapply<pack::template at<start>>;
+};
+}  // namespace forloop
+template <size_t start,
+          size_t end,
+          _Pack pack,
+          template <typename...> class constr = Pack>
+using for_loop =
+    typename forloop::impl<start, end, pack, constr, start <= end - 1>::type;
+
+// I'd like to figure out a clean way of doing this for arbitrary inputs,
+// i.e. tuples of inputs, but this `brute::pairs` example is supposed to
+// just be testing the intuition/basic constructs.
+// namespace brute::pairs {
+// namespace impl {
+// template <template <typename...> class constr,
+//           _Pack _Ts1,
+//           _Pack _Ts2,
+//           size_t counter1,
+//           size_t counter2>
+// struct pairs {
+//   static constexpr size_t _new_counter1 =
+//       (counter2 == 0) ? counter1 - 1 : counter1;
+//   static constexpr size_t _new_counter1 =
+//       (counter2 == 0) ? _Ts2::n : counter2 - 1;
+//
+//   using type =
+//       typename pairs<constr, _Ts1, _Ts2, _new_counter1,
+//       _new_counter2>::type::
+//           template postapply<constr<typename _Ts1::template at<counter1>,
+//                                     typename _Ts2::template at<counter2>>>;
+// };
+//
+// template <template <typename...> class constr, _Pack _Ts1, _Pack _Ts2>
+// struct pairs<constr, _Ts1, _Ts2, 0, 0> {
+//   using type = constr<>;
+// };
+//
+// }  // namespace impl
+//
+// template <template <typename...> class constr, _Pack _Ts1, _Pack _Ts2>
+// using pairs = typename impl::pairs<constr, _Ts1, _Ts2, _Ts1::n,
+// _Ts2::n>::type; }  // namespace brute::pairs
+//
+// template <_Pack _Ts1, _Pack _Ts2, template <typename...> class constr =
+// Pack> using brute_pairs = typename brute::pairs::pairs<constr, _Ts1, _Ts2>;
+
+// TODO
+
 namespace boolevals {
 namespace simplicial {
+
+// Condition (i) of `identities<...>()`
+namespace condition_i {
+
+template <size_t n, typename TruncSimpSet>
+constexpr bool _proper_dimensions() {
+  return TruncSimpSet::Set<n>::_dim == n;
+}
+
+}  // namespace condition_i
+
+// Condition (ii) of `identities<...>()`
+namespace condition_ii {
+
+template <typename simplex, size_t _face_codomain_max, size_t face_idx>
+constexpr bool _impl_face_codomains() {
+  return (simplex::face<face_idx> < _face_codomain_max)
+             ? _impl_face_codomains<simplex,
+                                    _face_codomain_max,
+                                    face_idx - 1>()
+             : false;
+}
+
+template <typename simplex, size_t _face_codomain_max>
+constexpr bool _impl_face_codomains<simplex, _face_codomain_max, 0>() {
+  return simplex::face<0> < _face_codomain_max;
+}
+
+template <typename simplex, size_t _face_codomain_max>
+constexpr bool _face_codomains() {
+  return _impl_face_codomains<simplex, _face_codomain_max, simplex::_dim>();
+}
+
+template <size_t n, typename TruncSimpSet, size_t simplex_idx>
+constexpr bool _impl_codomains() {
+  using set = typename TruncSimpSet::Set<n>;
+  using simplex = typename Set::materialize<(typename set::_)simplex_idx>;
+  return (_face_codomains<simplex, TruncSimpSet::Set<n - 1>::_n_primitives>())
+             ? _impl_codomains<n, TruncSimpSet, simplex_idx - 1>()
+             : false;
+}
+
+template <size_t n, typename TruncSimpSet>
+constexpr bool _impl_codomains<n, TruncSimpSet, 0>() {
+  using set = typename TruncSimpSet::Set<n>;
+  using simplex = typename Set::materialize<(typename set::_)simplex_idx>;
+  return _face_codomains<simplex, TruncSimpSet::Set<n - 1>::_n_primitives>();
+}
+
+// TODO: handle edge cases around `n`...?
+template <size_t n, typename TruncSimpSet>
+constexpr bool _codomains() {
+  return _impl_codomains<n,
+                         TruncSimpSet,
+                         TruncSimpSet::Set<n>::_n_primitives>();
+}
+
+}  // namespace condition_ii
+
+// Condition (iii) of `identities<...>()`
+namespace condition_iii {
+  // TODO: looping, conditions around `n` are complicated...
+}  // namespace condition_iii
 
 // the `_TruncatedSimplicialSet` concept says that
 // each `typename TruncSimpSet::Set<size_t>` is `_SimplexSetEnum`.
@@ -43,7 +229,7 @@ namespace simplicial {
 //         `0 <= i <= S::_dim` that
 //         `S::face<i> <= TruncSimpSet::Set<n - 1>::_n_primitives`.
 //         i.e. the degeneracy lands in the right place
-//         (TODO: for non-primitives, this might get odd...).
+//         (TODO: for non-primitives, this might get odd.. applies to iii too).
 //   (iii) We now know that the `S::face`s are well-defined, hence
 //         we need only check the simplicial identities, i.e. for each
 //         `0 <= i < j <= S::_dim` that
@@ -51,7 +237,7 @@ namespace simplicial {
 //         `TruncSimpSet<n - 1>::materialize<S::face<i>>::face<j - 1>`.
 template <size_t _dim, typename TruncSimpSet>
 constexpr bool identities() {
-  // TODO: link up
+  // TODO: link up and fix all the many many errors
   return true;
 }
 
